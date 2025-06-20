d870c5be6174313da0f0a9b28a269bce26464737
favouritetasty <19307110112@fudan.edu.cn>
2025-06-20 18:54:50 +0800

    init: heroui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+import { heroui } from "@heroui/theme";
+
 export default {
     purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        // "./node_modules/@heroui/theme/dist/components/button.js",
+    ],
     theme: {
-        extend: {
-            backgroundImage: {},
-            backgroundColor: {},
-        },
+        extend: {},
     },
-    plugins: [],
+    darkMode: "class",
+    plugins: [heroui()],
 };
