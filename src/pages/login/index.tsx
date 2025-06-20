cde26cd335bd679d7119d2f7f3b0b23cd2c8843d
favouritetasty <19307110112@fudan.edu.cn>
2025-06-20 20:09:37 +0800

    feat: login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,71 @@
-import { Image, Input } from "@heroui/react";
-import { FC } from "react";
+import { Button, Image, Input } from "@heroui/react";
+import { motion } from "framer-motion";
+import { FC, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router";
 
 export const Login: FC = () => {
     const { t } = useTranslation();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [disabled, setDisabled] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = async () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+            navigate("/home");
+        }, 1000);
+    };
+
+    useEffect(() => {
+        if (username.length > 0 && password.length > 0) {
+            setDisabled(false);
+        } else {
+            setDisabled(true);
+        }
+    }, [username, password]);
+
     return (
-        <div className="w-screen h-screen flex items-center justify-center">
+        <motion.div
+            className="w-screen h-screen flex items-center justify-center"
+            key="login"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+        >
             <div className="flex flex-col items-center justify-center w-96">
                 <Image className="mb-4" src="/logo.png" width={50} />
-                <div className="text-2xl mb-4">{t("login.title")}</div>
-                <Input label={t("login.username")} />
+                <div className="text-2xl mb-4 font-medium">
+                    {t("login.title")}
+                </div>
+                <Input
+                    className="mb-3"
+                    placeholder={t("login.username")}
+                    value={username}
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                />
+                <Input
+                    className="mb-3"
+                    placeholder={t("login.password")}
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+                <Button
+                    className="w-full bg-black text-white font-medium mb-[200px]"
+                    onPress={handleSubmit}
+                    isDisabled={disabled}
+                    isLoading={loading}
+                >
+                    {t("login.submit")}
+                </Button>
             </div>
-        </div>
+        </motion.div>
     );
 };
